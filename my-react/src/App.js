cb243170775bc25355eafe791d3cc783109453b5
Zobeir <rigi.zobir@yahoo.com>
2023-03-08 14:47:46 +0000

    handleClick

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import Button from "./button";
 import Mentors from "./Mentors";
+// import { ClickMe, Download, Subscribe } from "./ButtonFunctions";
 
+
+const ClickMe = () => {
+  console.log("I got Clicked");
+};
+
+const Download = () => {
+  console.log("Download ME ...");
+};
+
+const Subscribe = () => {
+  console.log("Subscrib Me ...");
+};
 
 
 function App() {
   return (
     <div>
       <Mentors />
+      <Button text="clickMe" handleClick={ClickMe} />
+      <Button text="Download" handleClick={Download} />
+      <Button text="Subscribe" handleClick={Subscribe} />
     </div>
   );
 }
---
+++ my-react/src/button.js
@@ -0,0 +1,7 @@
+const Button = ({ text, handleClick}) => {
+    console.log(handleClick)
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+
+export default Button ;
