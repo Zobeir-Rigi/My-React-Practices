4f083b04f73225aa6cb1ec82f3e942c0a5306ffb
Zobeir <rigi.zobir@yahoo.com>
2023-03-09 17:35:04 +0000

    i Added some info

--- my-react/src/fetch.js
+++ my-react/src/fetch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; // we need using useState to update our function
 function MartianPhotoFetcher() {
   const [marsPhotoData, setMarsPhotoData] = useState(null);
 
@@ -8,8 +8,8 @@ function MartianPhotoFetcher() {
     fetch(
       `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY`
     )
-      .then((res) => res.json())
-      .then((data) => {
+      .then((res) => res.json()) // we convert
+      .then((data) => { 
         setMarsPhotoData(data); // we update useState
       });
   }, []);
