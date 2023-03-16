4290589a892c4ba41fc031028e8c019939172d7c
Zobeir <rigi.zobir@yahoo.com>
2023-03-16 00:11:03 +0000

    ternary by &&

--- my-react/src/Ternary.js
+++ my-react/src/Ternary.js
@@ -4,11 +4,10 @@ const Ternary = () => {
   return (
     <Fragment>
       <h2>Ternary Operator</h2>
-      {user === "zobeir" ? (
-        <h2>Welcome to my page {user}</h2>
-      ) : (
-        <h2>Who are you</h2>
-      )}
+      {user === "zobeir" && <h2>Welcome to my page {user}</h2>}
+      
+        {/* <h2>Who are you</h2> */}
+      
     </Fragment>
   );
 };
