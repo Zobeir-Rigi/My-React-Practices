b6b66f468e9ee1dff4a053c346276f299229e83a
Zobeir <rigi.zobir@yahoo.com>
2023-03-16 21:43:29 +0000

    i put all the child components in the folder and i made a counter component

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import Button from "./button";
-import Mentors from "./Mentors";
-import MartianPhotoFetcher from "./fetch";
+import Button from "./Components/button";
+import Mentors from "./Components/Mentors";
+import MartianPhotoFetcher from "./Components/fetch";
 // import { ClickMe, Download, Subscribe } from "./ButtonFunctions";
-import Alert from "./Alert";
-import Counter from "./counter";
-import Highlighter from "./HighLighter"
-import Ternary from "./Ternary";
-import Loop from "./Loop";
+import Alert from "./Components/Alert";
+import Counter from "./Components/counter";
+import Highlighter from "./Components/HighLighter";
+import Ternary from "./Components/Ternary";
+import Loop from "./Components/Loop";
+import ToplearnCounter from "./Components/ToplearnCounter";
 
 const ClickMe = () => {
   console.log("I got Clicked");
@@ -21,14 +21,11 @@ const Subscribe = () => {
   console.log("Subscrib Me ...");
 };
 
-
-
-
-
 function App() {
   return (
     <div>
-      <Ternary  />
+    <ToplearnCounter />
+      <Ternary />
       <Loop />
       <p>
         Some text with a <Highlighter word="highlighted" color="red" /> word
---
+++ my-react/src/Components/ToplearnCounter.jsx
@@ -0,0 +1,4 @@
+const ToplearnCounter = () => {
+ return <p>Counter: {Math.floor( Math.random()*10)}</p> ;
+};
+export default ToplearnCounter ;
